a21d01971ef8f3d8ead8c0c9cfbc74a23e4367a0
Dennis Liang <Dennisliang01@gmail.com>
2022-02-10 12:54:56 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,9 +1,5 @@
-// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
@@ -12,19 +8,19 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if(nextOpenBracket == -1) {
-                break;
-            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            if (nextOpenBracket == -1) break;
+            
+            String closeBracket = markdown.substring(nextCloseBracket+1, nextCloseBracket+2); 
+            if (!closeBracket.equals("(")) break;
+            
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            String returnString = markdown.substring(openParen + 1, closeParen);
-            if(returnString.contains("(")) {
-                closeParen = markdown.indexOf(")", closeParen + 1);
-                returnString = markdown.substring(openParen + 1, closeParen);
-            }
-            toReturn.add(returnString);
+            //System.out.println("nextCloseB: " + nextCloseBracket + "; nextOpenB: "+ nextOpenBracket + "; currentIdx" + currentIndex +"\n");
+            if ( (nextOpenBracket ==0) || markdown.charAt(nextOpenBracket-1) != '!')
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            //System.out.println(currentIndex);
         }
         return toReturn;
     }
