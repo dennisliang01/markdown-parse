6cf38b0e42c5b30ba5ff4d1c7a0d25d0b1d9cead
Dennis Liang <Dennisliang01@gmail.com>
2022-02-10 12:55:58 -0800

    Revert to working version

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,3 +1,6 @@
+// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 public class MarkdownParse {
